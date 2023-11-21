9dda254f1608179198856a1f9e3c306a185dbf98
raturayya <raturayya08@gmail.com>
2023-11-21 10:18:09 +0700

    fix boilerplate

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  images: {
+    domains: [
+      's3.ap-southeast-1.amazonaws.com',
+      's3-ap-southeast-1.amazonaws.com',
+      'lpimgdev.chakrarewards.com',
+      'miniloyalty.chakra.uno'
+    ]
+  },
+  optimizeFonts: true,
+  devIndicators: {
+    buildActivity: false
+  }
+};
